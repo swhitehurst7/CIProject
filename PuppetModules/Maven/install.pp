9dad2c17ddb3233e63bd57cfd6db6c8ec60210b7
Rory101Bryett <rory.bryett@gmail.com>
2015-09-09 11:34:29 +0100

    updates install.pp

--- PuppetModules/Maven/install.pp
+++ PuppetModules/Maven/install.pp
@@ -1,5 +1,9 @@
 class maven::install {
 
+  $mavenDownload = "http://10.50.20.33:8080/aaron/downloads/apache-maven-3.3.3-bin.tar.gz"
+  $mavenTarball = "apache-maven-3.3.3-bin.tar.gz"
+  $mavenVersion = "apache-maven-3.3.3"
+
   Exec {
     path => ['/bin ', '/usr/bin', '/usr/sbin']
   }
@@ -14,24 +18,24 @@ class maven::install {
   
   exec { 'download' :
     cwd '/opt/maven',
-	command => 'wget http://10.50.15.14:8080/aaron/downloads/apache-maven-3.3.3-bin.tar.gz',
+	command => 'wget "${mavenDownload}"',
   }
   
   exec { 'extract' :
     cwd '/opt/maven',
-	command => 'tar -zxvf apache-maven-3.3.3-bin.tar.gz',
+	command => 'tar -zxvf "${mavenTarball}"',
   }
   
   file { '/usr/bin/mvn' :
     ensure => 'link',
-	target => '/opt/maven/apache-maven-3.3.3/bin/mvn'
+	target => '/opt/maven/"${mavenVersion}"/bin/mvn'
   }
   
   file { '/etc/profile.d/maven.sh' :
     ensure => 'present',
-	content => 'export MAVEN_HOME=/opt/apache-maven-3.3.3
-	export JAVA_HOME=/opt/jdk1.8.0_45
-	export JRE_HOME=/opt/jdk1.8.0_45/jre
+	content => 'export MAVEN_HOME=/opt/"${mavenVersion}"
+	# export JAVA_HOME=/opt/jdk1.8.0_45
+	# export JRE_HOME=/opt/jdk1.8.0_45/jre
 	export PATH=$PATH:%JAVA_HOME%/bin:%JRE_HOME%/bin:%MAVEN_HOME%/bin
 	export PATH',
   }
